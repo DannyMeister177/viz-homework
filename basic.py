5705b88217569c7350a1e9a970a32ad6774e7564
DannyMeister177 <danny.goyer177@gmail.com>
2020-03-04 20:08:26 -0500

    _

--- basic.py
+++ basic.py
@@ -1,5 +1,25 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
-pass
+os.makedirs('plots/basic', exist_ok=True)
+
+# Load the data
+df = pd.read_csv('data/WA_Fn-UseC_-HR-Employee-Attrition.csv')
+df.drop(['EmployeeCount', 'StandardHours'], axis=1, inplace=True)  # Column has no relevant info
+
+sns.set(style='darkgrid', palette='dark')
+
+
+def to_int():
+    pass
+
+
+# g = sns.pairplot(df[numerical], hue='Attrition', palette='seismic', diag_kind = 'kde',
+#                  diag_kws=dict(shade=True))
+# g.set(xticklabels=[])
+
+plt.savefig('plots/basic/scatterplot_.png')
+plt.clf()
